5b9a511771e5ba572f7f4da63802e40063a0b27c
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-11-11 12:43:15 +0100

    FAE-29 | test triggers

---
+++ lib/another_custom_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AnotherCustomWidget extends StatelessWidget {
+  const AnotherCustomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
