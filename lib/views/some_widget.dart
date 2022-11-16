2923c231564db43f49b29eb6f9af661901889477
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-11-16 14:23:47 +0100

    FAE-29 | add new widget

---
+++ lib/views/some_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SomeWidget extends StatelessWidget {
+  const SomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
